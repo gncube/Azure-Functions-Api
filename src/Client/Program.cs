a8459beea44d953ac893a6dba614313a06d2f375
Gerald Ncube <gerald.ncube@se.com>
2023-05-17 17:45:50 +0100

    Added Blazored LocalStorage

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Client;
 using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IBlogDataService, BlogDataService>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
--- src/Client/Services/BlogDataService.cs
+++ src/Client/Services/BlogDataService.cs
@@ -1,4 +1,6 @@
-﻿using GSN.Domain;
+﻿using Blazored.LocalStorage;
+using Client.Helper;
+using GSN.Domain;
 using System.Text.Json;
 
 namespace Client.Services;
@@ -6,13 +8,32 @@ namespace Client.Services;
 public class BlogDataService : IBlogDataService
 {
     private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorageService;
 
-    public BlogDataService(HttpClient httpClient)
+    public BlogDataService(HttpClient httpClient, ILocalStorageService localStorageService)
     {
         _httpClient = httpClient;
+        _localStorageService = localStorageService;
     }
     public async Task<IEnumerable<Blog>> GetAllBlogsAsync(bool refreshRequired = false)
     {
+        if (!refreshRequired)
+        {
+            bool blogExpirationKeyExists = await
+                _localStorageService.ContainKeyAsync(LocalStorageConstants.BlogListExpirationKey);
+            if (blogExpirationKeyExists)
+            {
+                DateTime blogListExpiration = await _localStorageService.GetItemAsync<DateTime>(LocalStorageConstants.BlogListExpirationKey);
+                if (blogListExpiration > DateTime.Now)
+                {
+                    if (await _localStorageService.ContainKeyAsync(LocalStorageConstants.BlogsListKey))
+                    {
+                        return await _localStorageService.GetItemAsync<IEnumerable<Blog>>(LocalStorageConstants.BlogsListKey);
+                    }
+                }
+            }
+        }
+
         var baseUrl = _httpClient.BaseAddress;
         using var responseStream = await _httpClient.GetStreamAsync($"api/blogs");
         using var jsonDoc = await JsonDocument.ParseAsync(responseStream);
@@ -20,6 +41,9 @@ public class BlogDataService : IBlogDataService
         var blogsArray = jsonDoc.RootElement.GetProperty("Value");
 
         var blogs = JsonSerializer.Deserialize<IEnumerable<Blog>>(blogsArray.GetRawText(), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+        await _localStorageService.SetItemAsync(LocalStorageConstants.BlogsListKey, blogs);
+        await _localStorageService.SetItemAsync(LocalStorageConstants.BlogListExpirationKey, DateTime.Now.AddMinutes(5));
 
         return blogs;
     }
