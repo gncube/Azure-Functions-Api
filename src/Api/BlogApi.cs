cea6aa95984ade2e859e1ff617024bc09d636076
Gerald Ncube <gerald.ncube@se.com>
2023-05-17 12:08:23 +0100

    Added BlogApi

--- src/Api/BlogApi.cs
+++ src/Api/BlogApi.cs
@@ -1,29 +1,109 @@
+using GSN.Application.Dtos;
+using GSN.Application.Interfaces;
+using GSN.Domain;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using System.Net;
+using Newtonsoft.Json;
 
 namespace Api;
 
 public class BlogApi
 {
     private readonly ILogger _logger;
+    private readonly IBlogRepository _repo;
 
-    public BlogApi(ILoggerFactory loggerFactory)
+    public BlogApi(ILoggerFactory loggerFactory, IBlogRepository repo)
     {
         _logger = loggerFactory.CreateLogger<BlogApi>();
+        _repo = repo;
     }
 
-    [Function("BlogApi")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    [Function(nameof(GetBlogs))]
+    public async Task<IActionResult> GetBlogs(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "blogs")] HttpRequestData req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation($"{nameof(GetBlogs)} ---> Getting all the blogs");
 
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        var blogs = _repo.GetAllBlogs();
 
-        response.WriteString("Welcome to Azure Functions!");
+        return new OkObjectResult(blogs);
+    }
 
-        return response;
+    [Function(nameof(GetBlogById))]
+    public async Task<IActionResult> GetBlogById(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "blogs/{id}")] HttpRequestData req, string id)
+    {
+        _logger.LogInformation($"{nameof(GetBlogById)} ---> Getting the blogToDelete with ID: {id}");
+
+        var blog = _repo.GetBlogById(id);
+        if (blog == null)
+        {
+            return new NotFoundResult();
+        }
+
+        return new OkObjectResult(blog);
+    }
+
+    [Function(nameof(CreateBlog))]
+    public async Task<IActionResult> CreateBlog(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "blogs")] HttpRequestData req)
+    {
+        _logger.LogInformation($"{nameof(CreateBlog)} ---> Processed the blogs");
+
+        var requestData = await new StreamReader(req.Body).ReadToEndAsync();
+        var blogCreateDto = JsonConvert.DeserializeObject<BlogCreateDto>(requestData);
+
+        var blog = new Blog
+        {
+            BlogName = blogCreateDto.BlogName,
+        };
+
+        _repo.AddBlog(blog);
+
+        return new OkObjectResult(blog);
+    }
+
+    [Function(nameof(PutBlog))]
+    public async Task<IActionResult> PutBlog(
+        [HttpTrigger(AuthorizationLevel.Function, "put", Route = "blogs/{id}")] HttpRequestData req, string id)
+    {
+        _logger.LogInformation($"{nameof(PutBlog)} ---> Updating the blogToDelete with ID: {id}");
+
+        var blog = _repo.GetBlogById(id);
+        if (blog == null)
+        {
+            _logger.LogWarning($"---> Blog could not be found!");
+            return new NotFoundResult();
+        }
+
+        var requestData = await new StreamReader(req.Body).ReadToEndAsync();
+        var blogUpdateDto = JsonConvert.DeserializeObject<BlogUpdateDto>(requestData);
+
+        blog.BlogName = blogUpdateDto.BlogName;
+        blog.Published = blogUpdateDto.Published;
+
+        _repo.UpdateBlog(blog);
+
+        return new OkObjectResult(blog);
+    }
+
+    [Function(nameof(DeleteBlog))]
+    public async Task<IActionResult> DeleteBlog(
+        [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "blogs/{id}")] HttpRequestData req, string id)
+    {
+        _logger.LogInformation($"{nameof(DeleteBlog)} ---> Deleting the blog with ID: {id}");
+
+        var blogToDelete = _repo.GetBlogById(id);
+        if (blogToDelete == null)
+        {
+            _logger.LogWarning($"---> Blog could not be found!");
+            return new NotFoundResult();
+        }
+
+        _repo.DeleteBlog(blogToDelete.Id);
+
+        return new OkResult();
     }
 }
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,21 @@
+using GSN.Application.Interfaces;
+using GSN.Infrastructure.Data;
+using GSN.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite("Data Source=database.sqlite");
+        });
+
+        services.AddScoped<IBlogRepository, BlogRepository>();
+    })
     .Build();
 
 host.Run();
